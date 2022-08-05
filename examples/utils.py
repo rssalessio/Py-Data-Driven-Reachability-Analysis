fd1dc20c7f9c5d8c7656383b75e0cea94fd66a09
Alessio Russo <alessior@kth.se>
2022-08-05 20:27:47 +0200

    finish concatenate zsonotope

--- examples/utils.py
+++ examples/utils.py
@@ -7,16 +7,18 @@ def concatenate_zonotope(zonotope: Zonotope, N: int) -> MatrixZonotope:
     assert N > 0 and isinstance(N, int), 'N must be a positiveinteger'
     dim_x = zonotope.dim
     C = np.tile(zonotope.center, (dim_x, N))
-    G = np.zeros(dim_x, N * N * zonotope.num_generators)
+    G = np.zeros((dim_x, N * N * zonotope.num_generators))
 
     for i in range(zonotope.num_generators):
         for j in range(N):
-            temp_G = np.zeros(dim_x, N)
-            G[:, j + i * N] = zonotope.Z[:, i]
-
-
-
+            G[:, i*N*N + j*N + j] = zonotope.generators[:, i]
+    
 
     return MatrixZonotope(C, G)
 
 
+W = Zonotope(np.array(np.zeros((2, 1))), 0.003 * np.ones((2, 1)))
+
+
+total_samples = 100
+M = concatenate_zonotope(W, total_samples)
